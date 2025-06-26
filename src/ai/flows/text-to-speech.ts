4673d1531a10729a09bd4c606faf17ab159d8c98
pavan sai teku <pavansaiteku2@gmail.com>
2025-06-26 14:55:28 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/ai/flows/text-to-speech.ts
+++ src/ai/flows/text-to-speech.ts
@@ -65,7 +65,7 @@ const textToSpeechFlow = ai.defineFlow(
         responseModalities: ['AUDIO'],
         speechConfig: {
           voiceConfig: {
-            prebuiltVoiceConfig: { voiceName: 'Electra' },
+            prebuiltVoiceConfig: { voiceName: 'Leda' },
           },
         },
       },
